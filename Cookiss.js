0b570316d8e347fbe723ede3e51382a6a5c3f403
emmet1234567890 <20emmetrosario@gmail.com>
2024-05-08 11:36:48 +0800

    Add files via upload

---
+++ Cookiss.js
@@ -0,0 +1,24 @@
+var navLinks = document.getElementById("navLinks");
+        function showMenu(){
+            navLinks.style.right ="0";
+        }
+        function hideMenu(){
+            navLinks.style.right ="-200px";
+        }
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                console.log(entry)
+                if (entry.isInteresting) {
+                    entry.target.classList.add('show');
+                } else {
+                    entry.target.classList.remove('show');
+                }
+
+            });
+        });
+
+
+
+      const hiddenElement = document.querySelectorAll('.Cool');
+      hiddenElement.forEach((el) => observer.observe(el));
